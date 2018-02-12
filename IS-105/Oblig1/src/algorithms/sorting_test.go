9165fe6c568a6b679d1de95db5d94ff798c2c928
seawaterz <35767365+seawaterz@users.noreply.github.com>
2018-02-12 12:31:48 +0100

    update sorting_test.go

--- IS-105/Oblig1/src/algorithms/sorting_test.go
+++ IS-105/Oblig1/src/algorithms/sorting_test.go
@@ -1,21 +1,15 @@
-package algorithms
+package main
 
+//importerer her
 import (
 	"time"
 	"testing"
 	"reflect"
-	"fmt"
 	"math/rand"
 )
 
-func main() {
-	fmt.Println("Bubble Sort test has passed")
-	b.StopTimer()
-}
 
-
-
-// https://golang.org/doc/effective_go.html#init
+// implementasjon av funksjoner init og perm som vil funksjone innen i test funkcsjoner
 func init() {
 	seed := time.Now().Unix()
 	rand.Seed(seed)
@@ -28,14 +22,15 @@ func perm(n int) (out []int) {
 	return
 }
 
-// Skriv "benchmark"-tester for benchmarkBSortModified funksjonen
-// Skriv en ny testfunksjon benchmarkBSortModified
+
+// Implementasjon av testfunksjoner for Bubbe_Sort_Modified algoritmen
+// Skrev en ny testfunksjon benchmark Bubbe_Sort_Modified
 
 func TestBSort(t *testing.T) {
 	values := []int{9, 1, 20, 3, 6, 7}
 	expected := []int{1, 3, 6, 7, 9, 20}
 
-	BubbleSort(values)
+	Bubble_sort_modified(values)
 
 	if !reflect.DeepEqual(values, expected) {
 		t.Fatalf("expected %d, actual is %d", 1, values[0])
@@ -59,9 +54,10 @@ func benchmarkBSort(i int, b *testing.B) {
 		b.StopTimer()
 		values := perm(i)
 		b.StartTimer()
-		BubbleSort(values)
+		Bubble_sort_modified(values)
 	}
 }
+
 
 // Implementasjon av testfunksjoner for Quicksort algoritmen
 func TestQSort(t *testing.T) {
@@ -94,53 +90,4 @@ func benchmarkQSort(i int, b *testing.B) {
 		b.StartTimer()
 		QuickSort(values)
 	}
-}
-
-// Les https://en.wikipedia.org/wiki/Bubble_sort
-// Implementering av Bubble_sort algoritmen
-func BubbleSort(a []int) {
-
-	// Skriv din kode her
-	for i:=0; i<len(a); i++ {
-		flag := 0;//Taking Flag Variable
-		for j:=0; j<6-i-1; j++ {
-			if a[j] > a[j+1] {
-				temp := a[j];
-				a[j] = a[j+1];
-				a[j+1] = temp;
-				flag = 1;//setting flag as 1, if swapping occurs
-			}
-		}
-		if flag == 0 {
-			break//breaking out for loop to reduce extra loops
-		}
-	}
-
-}
-
-
-// Implementering av Quicksort algoritmen
-func QuickSort(values []int) {
-	qsort(values, 0, len(values)-1)
-}
-
-func qsort(values []int, l int, r int) {
-	if l >= r {
-		return
-	}
-
-	pivot := values[l]
-	i := l + 1
-
-	for j := l; j <= r; j++ {
-		if pivot > values[j] {
-			values[i], values[j] = values[j], values[i]
-			i++
-		}
-	}
-
-	values[l], values[i-1] = values[i-1], pivot
-
-	qsort(values, l, i-2)
-	qsort(values, i, r)
 }
