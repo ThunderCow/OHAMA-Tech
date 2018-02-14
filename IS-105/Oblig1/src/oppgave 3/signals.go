4c200ad1d71ab3bc7eab335bc55302d6b1509326
Ole Morten <31931528+ThunderCow@users.noreply.github.com>
2018-02-15 00:06:09 +0100

    Update signals.go

--- IS-105/Oblig1/src/oppgave 3/signals.go
+++ IS-105/Oblig1/src/oppgave 3/signals.go
@@ -40,9 +40,9 @@ import (
 
 	
 /*
-Programmet bruker 6.6 MG ram og nesten ingen CPU. Dette så vi vet å kjøre programmet i cmd,
-og deretter sjekket vi hvor mye ram og cpu cmd brukte når programmet var under eksekvering. 
-Når programmet var ikke i eksekvering bruktte cmd kun 6.6 MG RAM. Under ekskvering av pro-
-grammet cmd 13.2 RAM og ikke noe CPU.
+Programmet bruker 6.6 MG ram og nesten ingen CPU. Dette så vi ved å kjøre programmet i cmd,
+deretter sjekket vi hvor mye ram og cpu cmd brukte når programmet var under eksekvering. 
+Når programmet var ikke i eksekvering brukte cmd kun 6.6 MG RAM. Under ekskvering av programmet
+cmd 13.2 RAM og ikke noe CPU.
 */
 
