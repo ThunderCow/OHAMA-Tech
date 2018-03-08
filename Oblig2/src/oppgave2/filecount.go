293bc855a93d43e774c1b8a305430d99155e5a5a
Ole Morten <31931528+ThunderCow@users.noreply.github.com>
2018-03-08 23:09:57 +0100

    Update filecount.go
    Need some cleaning and commenting.

--- Oblig2/src/oppgave2/filecount.go
+++ Oblig2/src/oppgave2/filecount.go
@@ -1,16 +1,15 @@
-/* The code lines 114-119 & 131-136 are not completed yet(*to take out most common runes from a file & look values
-from highest to lowest )but major part has been done.*/
 package main
 
 import (
-	"fmt"
-	"regexp"
-	"sort"
-	"strings"
-	"os"
-	"io/ioutil"
 	"bufio"
+	"fmt"
+	"os"
+	"sort"
+	"io/ioutil"
+	"strings"
+	"regexp"
 )
+
 //Linecounter for the file given as a argument
 func linjeteller(filename string) (int64, error) {
 	lc := int64(0)
@@ -18,121 +17,106 @@ func linjeteller(filename string) (int64, error) {
 	if err != nil {
 		return 0, err
 	}
-	//to close a file
 	defer f.Close()
-	//scaning for line counter
 	s := bufio.NewScanner(f)
 	for s.Scan() {
 		lc++
 	}
 	return lc, s.Err()
 }
-//to find number of words in a file
-func word_count(words []string) map[string]int {
-	// iterate over the slice of words and populate
-	// the map with word:frequency pairs
-	// (where word is the key and frequency is the value)
-	word_freq := make(map[string]int)
-	// range string slice gives index, word pairs
-	// index is not needed, so use blank identifier _
-	for _, word := range words {
-		// check if word (the key) is in already in the map
-		_, ok := word_freq[word]
-		// if true add 1 to frequency (value of map)
-		// else start frequency at 1
-		if ok == true {
-			word_freq[word] += 1
-		} else {
-			word_freq[word] = 1
-		}
-	}
-	return word_freq
-}
-//declaring type structures
-type word_struct struct {
-	freq int
-	word string
-}
 
-// word_struct will be displayed in this format
-func (p word_struct) String() string {
-	return fmt.Sprintf("%3d   %s", p.freq, p.word)
-}
-
-// by_freq implements sort.Interface for []word_struct based on the freq field
-type by_freq []word_struct
-
-func (a by_freq) Len() int           { return len(a) }
-func (a by_freq) Swap(i, j int)      { a[i], a[j] = a[j], a[i] }
-func (a by_freq) Less(i, j int) bool { return a[i].freq < a[j].freq }
-
-// by_word implements sort.Interface for []word_struct based on the word field
-type by_word []word_struct
-
-func (a by_word) Len() int           { return len(a) }
-func (a by_word) Swap(i, j int)      { a[i], a[j] = a[j], a[i] }
-func (a by_word) Less(i, j int) bool { return a[i].word < a[j].word }
-
-func main() {
-	//to take argument
-	filename := os.Args[1]
-	lc, err := linjeteller(filename)
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	//print out file info
-	fmt.Println("Information about", filename+":\n",)
-	fmt.Println("Number of lines in file:", lc)
-	
-	//code to present data in rune form and find most common runes
+/*func bokstavteller() {
 	filen := os.Args[1]
-	read, err := ioutil.ReadFile(filen)
+	b, err := ioutil.ReadFile(filen)
 	if err != nil {
 		fmt.Print(err)
 	}
+
+	asciiStr := b
+	asciiBytes := []byte(asciiStr)
+
+	fmt.Printf("test string=%v, bytes=%v\n", asciiStr, asciiBytes)
+}*/
+
+func char_count(chars []string) map[string]int {
+	// iterate over the slice of charcters and populate
+	// the map with char:frequency pairs
+	// (where char is the key and frequency is the value)
+	char_freq := make(map[string]int)
+	// range string slice gives index, char pairs
+	// index is not needed, so use blank identifier _
+	for _, char := range chars {
+		// check if char (the key) is in already in the map
+		_, ok := char_freq[char]
+		// if true add 1 to frequency (value of map)
+		// else start frequency at 1
+		if ok == true {
+			char_freq[char] += 1
+		} else {
+			char_freq[char] = 1
+		}
+	}
+	return char_freq
+}
+
+type char_struct struct {
+	freq int
+	char string
+}
+
+// by_freq implements sort.Interface for []char_struct based on the freq field
+type by_freq []char_struct
+
+func (a by_freq) Len() int           { return len(a) }
+func (a by_freq) Swap(i, j int)      { a[i], a[j] = a[j], a[i] }
+func (a by_freq) Less(i, j int) bool { return a[i].freq > a[j].freq }
+
+// by_char implements sort.Interface for []char_struct based on the word field
+type by_char []char_struct
+
+func (a by_char) Len() int           { return len(a) }
+func (a by_char) Swap(i, j int)      { a[i], a[j] = a[j], a[i] }
+func (a by_char) Less(i, j int) bool { return a[i].char > a[j].char }
+
+func main() {
+	filen := os.Args[1]
+	read, err := ioutil.ReadFile(filen)
+	lc, err := linjeteller(filen)
+	if err != nil {
+		fmt.Print(err)
+	}
+
+	fmt.Println("Information about", filen+":\n",)
+	fmt.Println("Number of lines in file:", lc)
+
 	text := string(read)
+
 	// change text to all lower characters
 	text = strings.ToLower(text)
 
-	// match whole words, removes any punctuation/whitespace
-	re := regexp.MustCompile(`[^\s-]`)
-	words := re.FindAllString(text, -1)
+	// match chars, removes any punctuation/whitespace
+	re := regexp.MustCompile(`[^a\s-]`)
+	chars := re.FindAllString(text, -1)
 
-	// word_map() returns a map of word:frequency pairs
-	word_map := word_count(words)
+	// char_map() returns a map of char:frequency pairs
+	char_map := char_count(chars)
 
 	// convert the map to a slice of structures for sorting
-	// create pointer to an instance of word_struct
-	pws := new(word_struct)
-	struct_slice := make([]word_struct, len(word_map))
+	// create pointer to an instance of char_struct
+	pws := new(char_struct)
+	struct_slice := make([]char_struct, len(char_map))
 	ix := 0
-	maxK := ""
-	maxV := 0
-	for key, val := range word_map {
+	for key, val := range char_map {
 		pws.freq = val
-		pws.word = key
-		if pws.freq > maxV {
-			maxV = pws.freq
-			//temp := maxV
-			//maxV = 0
-			maxK = pws.word
-		}
+		pws.char = key
 		struct_slice[ix] = *pws
 		ix++
 	}
-
+	fmt.Println("Most common runes:\n")
 	// sorting slice of structers by field freq in place
 	sort.Sort(by_freq(struct_slice))
-	for r := 0; r < len(struct_slice); r++ {
-		fmt.Println("Rune:", struct_slice[r].word, "Count:", struct_slice[r].freq)
 
-	}
-	//Printing most common runes in a file
-	fmt.Println("Most common runes are:\n")
-	for v := 0; v < 5; v++ {
-		fmt.Println("Rune:", maxK, "Count:", maxV)
-		maxV--
-		//strings.Trim(pws.word, maxK)
+	for i, ix := 1, 0; i < 6; i, ix = i+1, ix+1 {
+		fmt.Println(i, "Rune:", struct_slice[ix].char, " Count:", struct_slice[ix].freq)
 	}
 }
