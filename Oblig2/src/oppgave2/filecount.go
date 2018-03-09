e3ff2deacc080143f05c363861d3a711414fcf7d
Ole Morten <31931528+ThunderCow@users.noreply.github.com>
2018-03-09 13:15:02 +0100

    Update filecount.go

--- Oblig2/src/oppgave2/filecount.go
+++ Oblig2/src/oppgave2/filecount.go
@@ -8,10 +8,11 @@ import (
 	"io/ioutil"
 	"strings"
 	"regexp"
+	"strconv"
 )
 
-//Linecounter for the file given as a argument
-func linjeteller(filename string) (int64, error) {
+// Linecounter for the file given as a argument
+func linecounter(filename string) (int64, error) {
 	lc := int64(0)
 	f, err := os.Open(filename)
 	if err != nil {
@@ -31,17 +32,17 @@ func linjeteller(filename string) (int64, error) {
 
 
 func char_count(chars []string) map[string]int {
-	// iterate over the slice of charcters and populate
-	// the map with char:frequency pairs
+	// Iterate over the slice of characters and populate
+	// The map with char:frequency pairs
 	// (where char is the key and frequency is the value)
 	char_freq := make(map[string]int)
 	// range string slice gives index, char pairs
-	// index is not needed, so use blank identifier _
+	// Index is not needed, so use blank identifier _
 	for _, char := range chars {
-		// check if char (the key) is in already in the map
+		// Check if char (the key) is in already in the map
 		_, ok := char_freq[char]
-		// if true add 1 to frequency (value of map)
-		// else start frequency at 1
+		// If true add 1 to frequency (value of map)
+		// Else start frequency at 1
 		if ok == true {
 			char_freq[char] += 1
 		} else {
@@ -51,7 +52,7 @@ func char_count(chars []string) map[string]int {
 	return char_freq
 }
 
-// define what data type we will use
+// Define what data type we will use
 type char_struct struct {
 	freq int
 	char string
@@ -66,30 +67,33 @@ func (a by_freq) Less(i, j int) bool { return a[i].freq > a[j].freq }
 
 
 func main() {
+	// Get a file as argument from terminal, reads the content
 	filen := os.Args[1]
 	read, err := ioutil.ReadFile(filen)
-	lc, err := linjeteller(filen)
+	lc, err := linecounter(filen)
 	if err != nil {
 		fmt.Print(err)
 	}
 
+	// Print the filename and linecount
 	fmt.Println("Information about", filen+":\n",)
 	fmt.Println("Number of lines in file:", lc, "\n")
 
+	// Defines the text for char_count function, read from the file given in terminal as argument
 	text := string(read)
 
 	// change text to all lower characters
 	text = strings.ToLower(text)
 
-	// match chars, removes any punctuation/whitespace
+	// Match chars, removes any punctuation/whitespace
 	re := regexp.MustCompile(`[^a\s-]`)
 	chars := re.FindAllString(text, -1)
 
 	// char_map() returns a map of char:frequency pairs
 	char_map := char_count(chars)
 
-	// convert the map to a slice of structures for sorting
-	// create pointer to an instance of char_struct
+	// Convert the map to a slice of structures for sorting
+	// Create pointer to an instance of char_struct
 	pws := new(char_struct)
 	struct_slice := make([]char_struct, len(char_map))
 	ix := 0
@@ -101,9 +105,10 @@ func main() {
 	}
 	fmt.Println("Most common runes:\n")
 
-	// sorting slice of structers by field freq in place
+	// Sorting slice of structers by field freq in place
 	sort.Sort(by_freq(struct_slice))
 	for i, ix := 1, 0; i < 6; i, ix = i+1, ix+1 {
-		fmt.Println(i,".", "Rune:", struct_slice[ix].char, ", Count:", struct_slice[ix].freq)
-	}
+		randomString := strconv.Itoa(i) + "."
+		fmt.Println(strings.Replace(randomString, " ", "", -1),  "Rune:", struct_slice[ix].char, ", Count:", struct_slice[ix].freq)
+		}
 }
