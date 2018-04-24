1fe441eadc5f35e635b47f9c0738fb5e79748949
MahamedYusuf-UIA <32332090+MahamedYusuf-UIA@users.noreply.github.com>
2018-04-24 23:57:07 +0200

    Update udp_client.go

--- Oblig3/Oppgave 3/udp_client.go
+++ Oblig3/Oppgave 3/udp_client.go
@@ -17,14 +17,6 @@ func main() {
 	// read in input from stdin
 	reader := bufio.NewReader(os.Stdin)
 
-	for {
-		fmt.Print("Text to send: ")
-		text, _ := reader.ReadString('\n')
-		// send to socket
-		fmt.Fprintf(conn, text + "\n")
-		// listen for reply
-		message, _ := bufio.NewReader(conn).ReadString('\n')
-		fmt.Print("Message from server: "+message)
-	}
+	
 	conn.Close()
 }
